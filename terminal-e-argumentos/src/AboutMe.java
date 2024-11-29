99e0cd2fb2400eb254b2b74ae8a7900a35c13211
Luan <luandsvt@gmail.com>
2024-11-29 09:36:06 -0300

    Terminal e Argumentos

--- terminal-e-argumentos/src/AboutMe.java
+++ terminal-e-argumentos/src/AboutMe.java
@@ -1,8 +1,10 @@
 public class AboutMe {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         String nome = args[0];
         String sobrenome = args[1];
+        @SuppressWarnings("UnnecessaryTemporaryOnConversionFromString")
         int idade = Integer.valueOf(args[2]);
+        @SuppressWarnings("UnnecessaryTemporaryOnConversionFromString")
         double altura = Double.valueOf(args[3]);
 
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
