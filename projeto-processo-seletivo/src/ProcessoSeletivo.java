b1cd76c4499da43f4ae34628d731f95b8eba2453
Luan <luandsvt@gmail.com>
2024-12-01 17:54:54 -0300

    projeto-processo-seletivo pt3

--- projeto-processo-seletivo/src/ProcessoSeletivo.java
+++ projeto-processo-seletivo/src/ProcessoSeletivo.java
@@ -4,7 +4,25 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        String[] candidatos = {"Felipe", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int i = 0; i < candidatos.length; i++){
+            System.out.println("O candidato de número " + (i+1) + " é " + candidatos[i]);
+        }
+
+        System.out.println("");
+        
+        System.out.println("Forma abreviada de interação for each");
+
+        for(String candidato : candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos(){
@@ -32,7 +50,6 @@ public class ProcessoSeletivo {
     private static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
     }
-
 
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
