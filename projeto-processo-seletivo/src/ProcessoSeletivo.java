bc2c5bb86a1632ccf1918c3a084397100005bb88
Luan <luandsvt@gmail.com>
2024-12-01 17:47:52 -0300

    projeto-processo-seletivo pt2

--- projeto-processo-seletivo/src/ProcessoSeletivo.java
+++ projeto-processo-seletivo/src/ProcessoSeletivo.java
@@ -1,10 +1,38 @@
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
+
+    static void selecaoCandidatos(){
+        String[] candidatos = {"Felipe", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) { 
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+            
+            if(salarioBase >= salarioPretendido){
+                candidatosSelecionados++;
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+            }
+
+            candidatosAtual++;
+        }
+    }
+
+    private static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
+    }
+
 
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
@@ -16,4 +44,6 @@ public class ProcessoSeletivo {
             System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
         }
     }
+
+    
 }
